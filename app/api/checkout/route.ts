d868fe00d0c2b703cdbe9b2b0af6e6139b6e2037
Faker389 <161638132+Faker389@users.noreply.github.com>
2026-02-03 20:47:21 +0100

    Add 'p24' to payment method types for checkout

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: Request) {
 
   const session = await stripe.checkout.sessions.create({
     mode: "payment",
-    payment_method_types: ["card", "blik"],
+    payment_method_types: ["card", "blik","p24"],
     customer_email: email,
 
     line_items: [
