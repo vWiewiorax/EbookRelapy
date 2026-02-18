cd2f169c622756e15e2a06e09dcb93ae37f9e785
Faker389 <michal200614@gmail.com>
2026-02-18 21:26:51 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,9 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import { Analytics } from "@vercel/analytics/next"
-import dynamic from "next/dynamic";
 
-const CookieWindow = dynamic(
-  () => import("./components/cookies/window"),
-  { ssr: false }
-);
 import Script from "next/script"
+import CookieWindow from "./components/cookies/window"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
